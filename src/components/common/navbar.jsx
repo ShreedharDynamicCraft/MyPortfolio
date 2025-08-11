f6595d300272793e7d8c8e7cf454060b6a1a4401
Shreedhar <shreedharanandji@gmail.com>
2025-08-11 18:11:29 +0530

    made navbar fixed

--- src/components/common/navbar.jsx
+++ src/components/common/navbar.jsx
@@ -2,29 +2,34 @@ import LetterD from '../../assets/letterD.png'
 
 const Navbar = () => {
   return (
-    <div className="relative w-full">
+    <>
       {/* Navbar */}
-      <nav className="flex flex-col md:flex-row justify-around items-center px-4 md:px-8 py-4 w-full">
-        <div className="flex items-center text-xl md:text-2xl font-normal mb-4 md:mb-0">
-          <img src={LetterD} alt="D" className="bg-blue-50 w-10 h-10 mr-1" />
-          <span className="font-bevietnam">Divyansh Gupta</span>
-        </div>
-        <ul className="flex flex-row gap-6 md:gap-12 text-lg">
-          <li>
-            <a href="/" className="text-black font-light hover:border-b-2 hover:border-black transition-all duration-300 px-1 py-1">Home</a>
-          </li>
-          <li>
-            <a href="/" className="text-black font-light hover:border-b-2 hover:border-black transition-all duration-300 px-1 py-1">Contact Us</a>
-          </li>
-          <li>
-            <a href="/" className="text-black font-light hover:border-b-2 hover:border-black transition-all duration-300 px-1 py-1">Projects</a>
-          </li>
-        </ul>
-      </nav>
+      <div className="fixed top-0 left-0 w-full z-50 bg-blue-50 shadow">
+        <nav className="flex flex-col md:flex-row justify-around items-center px-4 md:px-8 py-4 w-full">
+          <div className="flex items-center text-xl md:text-2xl font-normal mb-4 md:mb-0">
+            <img src={LetterD} alt="D" className="bg-blue-50 w-10 h-10 mr-1" />
+            <span className="font-bevietnam">Divyansh Gupta</span>
+          </div>
+          <ul className="flex flex-row gap-6 md:gap-12 text-lg">
+            <li>
+              <a href="/" className="text-black font-light hover:border-b-2 hover:border-black transition-all duration-300 px-1 py-1">Home</a>
+            </li>
+            <li>
+              <a href="/" className="text-black font-light hover:border-b-2 hover:border-black transition-all duration-300 px-1 py-1">Contact Us</a>
+            </li>
+            <li>
+              <a href="/" className="text-black font-light hover:border-b-2 hover:border-black transition-all duration-300 px-1 py-1">Projects</a>
+            </li>
+          </ul>
+        </nav>
 
-      {/* Bottom line centered */}
-      <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-1/3 border-b-2 border-black"></div>
-    </div>
+        {/* Bottom line centered */}
+        {/* <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-1/3 border-b-2 border-black"></div> */}
+      </div>
+
+      {/* Spacer so content doesn't hide behind navbar */}
+      <div className="h-20"></div>
+    </>
   )
 }
 
