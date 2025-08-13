c61725c6c53521dec92605de52bf1b90e3ece1a9
Shreedhar <shreedharanandji@gmail.com>
2025-08-13 21:14:27 +0530

    segregated the techstack page code and data

---
+++ src/data/TechStacks.js
@@ -0,0 +1,163 @@
+const techStacks = [
+  // Programming Languages
+  {
+    id: 1,
+    name: "C++",
+    category: "Programming Languages",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg",
+    color: "from-blue-600 to-blue-800",
+  },
+  {
+    id: 2,
+    name: "JavaScript",
+    category: "Programming Languages",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+    color: "from-yellow-400 to-yellow-600",
+  },
+  {
+    id: 3,
+    name: "TypeScript",
+    category: "Programming Languages",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+    color: "from-blue-500 to-blue-700",
+  },
+  {
+    id: 4,
+    name: "Python",
+    category: "Programming Languages",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
+    color: "from-green-400 to-blue-600",
+  },
+  {
+    id: 5,
+    name: "C",
+    category: "Programming Languages",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg",
+    color: "from-blue-700 to-blue-900",
+  },
+  {
+    id: 6,
+    name: "PHP",
+    category: "Programming Languages",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg",
+    color: "from-purple-600 to-indigo-700",
+  },
+  {
+    id: 20,
+    name: "Bash",
+    category: "Programming Languages",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bash/bash-original.svg",
+    color: "from-gray-600 to-gray-800",
+  },
+
+  // Full-Stack Development
+  {
+    id: 7,
+    name: "React.js",
+    category: "Full-Stack Development",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+    color: "from-cyan-400 to-blue-500",
+  },
+  {
+    id: 8,
+    name: "Next.js",
+    category: "Full-Stack Development",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg",
+    color: "from-gray-800 to-black",
+  },
+  {
+    id: 9,
+    name: "HTML+CSS",
+    category: "Full-Stack Development",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+    color: "from-orange-500 to-red-600",
+  },
+  {
+    id: 10,
+    name: "Tailwind CSS",
+    category: "Full-Stack Development",
+    icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/tailwindcss/tailwindcss-original.svg",
+    color: "from-cyan-400 to-teal-600",
+  },
+  {
+    id: 11,
+    name: "Bootstrap",
+    category: "Full-Stack Development",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg",
+    color: "from-purple-600 to-purple-800",
+  },
+  {
+    id: 12,
+    name: "Redux",
+    category: "Full-Stack Development",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg",
+    color: "from-purple-500 to-purple-700",
+  },
+  {
+    id: 13,
+    name: "Node.js",
+    category: "Full-Stack Development",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+    color: "from-green-500 to-green-700",
+  },
+  {
+    id: 14,
+    name: "Express.js",
+    category: "Full-Stack Development",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg",
+    color: "from-gray-700 to-gray-900",
+  },
+
+  // Database & Tools
+  {
+    id: 15,
+    name: "MongoDB",
+    category: "Database & Tools",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg",
+    color: "from-green-600 to-green-800",
+  },
+  {
+    id: 16,
+    name: "MySQL",
+    category: "Database & Tools",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg",
+    color: "from-blue-600 to-orange-500",
+  },
+  // {
+  //   id: 21,
+  //   name: "SQL",
+  //   category: "Database & Tools",
+  //   icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg",
+  //   color: "from-blue-500 to-blue-700"
+  // },
+  {
+    id: 17,
+    name: "Git",
+    category: "Database & Tools",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+    color: "from-red-500 to-red-700",
+  },
+  {
+    id: 18,
+    name: "GitHub",
+    category: "Database & Tools",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
+    color: "from-gray-800 to-black",
+  },
+  {
+    id: 19,
+    name: "Linux",
+    category: "Database & Tools",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg",
+    color: "from-yellow-500 to-black",
+  },
+  {
+    id: 22,
+    name: "JWT",
+    category: "Database & Tools",
+    icon: "https://cdn.worldvectorlogo.com/logos/jwt-3.svg",
+    color: "from-pink-500 to-purple-600",
+  },
+];
+
+export default techStacks;
