8bb76a12ef59877a3fdab462dfb84043772f2cc1
Shreedhar <shreedharanandji@gmail.com>
2025-08-13 15:12:25 +0530

    link change for resume

--- src/data/DownloadLinks.js
+++ src/data/DownloadLinks.js
@@ -1,4 +1,4 @@
 const links = {
-  downloadCV: "https://drive.google.com/uc?export=download&id=1zZ-d0FeDMUWACv2tULLWuVDcAijhvdit",
+  downloadCV: "https://drive.google.com/uc?export=download&id=1WugMV5NpwlaR8nRydFLprpzCD45vSCTp",
 };
 export default links;
