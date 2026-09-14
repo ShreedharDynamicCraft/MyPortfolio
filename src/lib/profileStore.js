008bbd0a4a9238c13b912b39ed6f2676d9993ce2
Shreedhar Anand <Shreedharanand@urbancompany.com>
2026-09-15 04:04:30 +0530

    reset edit-profile admin password

--- src/lib/profileStore.js
+++ src/lib/profileStore.js
@@ -11,7 +11,7 @@ import {
 
 const STORAGE_KEY = 'portfolio.data.v1'
 
-const DEFAULT_PW_HASH = '28x7mgmkfd'
+const DEFAULT_PW_HASH = '2d8xy2g0qxn'
 
 function cyrb53(str, seed = 0) {
   let h1 = 0xdeadbeef ^ seed
